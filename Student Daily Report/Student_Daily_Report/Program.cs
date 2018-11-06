5ad60e86be9f971158c3476ff7ea9104fd3ede49
Tyler Mc <tmcintyre802@gmail.com>
2018-11-05 18:52:40 -0700

    Student daily report
    My student daily report file

---
+++ Student Daily Report/Student_Daily_Report/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StudentDailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student daily Report");
+
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+
+
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            int pageNum = Convert.ToInt32(pageNumber);
+
+            Console.WriteLine("Do you need help with anything? Please answer \"True\" or \"False\"");
+            string questionHelp = Console.ReadLine();
+            bool quesHelp = Convert.ToBoolean(questionHelp);
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics");
+            string questionExperiences = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string questionFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string numOfHours = Console.ReadLine();
+            int numHours = Convert.ToInt32(numOfHours);
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+
+        }
+    }
+}
