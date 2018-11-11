b26fd6cc4b5d4c4769fa02cd952c00e76fc95f8a
Tyler Mc <tmcintyre802@gmail.com>
2018-11-11 10:43:24 -0700

    Void Methods
    a practice app for calling and using void methods

---
+++ VoidMethods/VoidMethods/MyMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethods
+{
+    public class MyMethod
+    {
+        public static void Add(int a, int b) {
+            a++;
+            Console.WriteLine(b);
+            Console.WriteLine(a);
+            return;
+        }
+         
+    }
+}
---
+++ VoidMethods/VoidMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyMethod.Add(a: 3, b: 7);
+            Console.ReadLine();
+        }
+    }
+}
+
+/*
+1. Have the method do a math operation on the first integer and display the second integer to the screen.
+
+2. In the Main() method of the console app, instantiate the class.
+
+3. Call the method in the class, passing in two numbers.
+
+4. Call the method in the class, specifying the parameters by name.
+*/
