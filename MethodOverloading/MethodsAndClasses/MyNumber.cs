8f72a9fd7789e57d31ce4ebbfb95eada8f785bc7
Tyler Mc <tmcintyre802@gmail.com>
2018-11-11 14:09:55 -0700

    Method Overloading
    This is a practice console app for learning method overloading

---
+++ MethodOverloading/MethodsAndClasses/MyNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndClasses
+{
+    public static class MyNumber
+    {
+        public static int Math(int a, int b)
+        {
+            Console.WriteLine(a + b);
+            return a + b;
+        }
+
+        public static int Math(decimal a, decimal b)
+        {
+            // Convert var a from decimal to int
+            int num1 = Convert.ToInt32(a);
+
+            // Convert var b from decimal to int
+            int num2 = Convert.ToInt32(b);
+           
+            return num1 - num2;
+
+        }
+
+        public static int Math(string a, string b) 
+        {
+            string a = Console.ReadLine();
+            int num1 = Convert.ToInt32(a);
+
+            string b = Console.ReadLine();
+            int num2 = Convert.ToInt32(b);
+
+
+            return num1 / num2;
+        }
+
+
+    }
+}
---
+++ MethodOverloading/MethodsAndClasses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Method Add
+            MyNumber.Math(a: 3,b: 12);
+            Console.ReadLine();
+
+            //Method Subtract
+            MyNumber.Math(a: 7,b: 2);
+            Console.ReadLine();
+
+            //Method Divide
+            MyNumber.Math(6, 9);
+            Console.ReadLine();
+
+        }
+    }
+}
+
