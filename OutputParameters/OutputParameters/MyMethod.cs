64d54a94ffc0f786b5218f65053064a348de2dde
Tyler Mc <tmcintyre802@gmail.com>
2018-11-12 17:01:51 -0700

    Output parameters on methods
    This is a console app to practice putting output parameters onto a method

---
+++ OutputParameters/OutputParameters/MyMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputParameters
+{
+    public static class MyMethod
+    {
+
+        public static void Method(int a, out int res) {
+            res = a / 2;
+        }
+
+        public static void Method(decimal b, out decimal res2)
+        {
+            res2 = b / 2;
+        }
+
+    }
+}
---
+++ OutputParameters/OutputParameters/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int res = 2;
+
+            // user chooses a number to divide by
+            Console.WriteLine("Choose a number.");
+            // string conversion to int to pass through method
+            string userInput = Console.ReadLine();
+            int userNum = Convert.ToInt32(userInput);
+
+            // call method on user choosen number
+            MyMethod.Method(userNum, out res);
+            //log result to console
+            Console.WriteLine(res);
+            Console.ReadLine();
+
+
+            // methoded was overloaded. Call the second method.
+            decimal res2 = 2;
+
+            // user chooses another number to divide by
+            Console.WriteLine("Choose a number.");
+            // string conversion to int to pass through method
+            string userInput2 = Console.ReadLine();
+            int userNum2 = Convert.ToInt32(userInput);
+
+            // call method on user choosen number
+            MyMethod.Method(userNum2, out res2);
+            //log result 2 to console
+            Console.WriteLine(res2);
+            Console.ReadLine();
+        }
+    }
+}
+
