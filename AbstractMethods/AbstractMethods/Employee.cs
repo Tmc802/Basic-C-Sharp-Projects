59cc876f9b210c5da0a8306986e7d6565927ba57
Tyler Mc <tmcintyre802@gmail.com>
2018-11-13 20:33:45 -0700

    Abstract Methods
    A console app to practice using abstract methods

---
+++ AbstractMethods/AbstractMethods/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractMethods
+{
+    public class Employee : Person
+    {
+        public override void SayName() {
+            Console.WriteLine(FirstName + LastName);
+        }
+
+    }
+}
+/*
+5. Inside the Main() method, instantiate an Employee object with firstName "Sample" and lastName "Student". Call the SayName() method on the object.
+*/
