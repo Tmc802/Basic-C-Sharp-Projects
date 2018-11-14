2dd62539ebc6cc4d992424dbb20be4ac1f6f8e34
Tyler Mc <tmcintyre802@gmail.com>
2018-11-13 19:01:56 -0700

    Inheritance App
    A Console app to practice inherating properties across multiple classes

---
+++ PersonProject/PersonProject/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonProject
+{
+    public class Person
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName() {
+            Console.WriteLine("Name: " + FirstName + LastName);
+        }
+    }
+}
+
+/* 
+ * 
+5. Call the superclass method SayName() on the Employee object.
+ */
---
+++ PersonProject/PersonProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate class Person
+            Person person = new Person();
+            person.FirstName = "Tyler ";
+            person.LastName = "McIntyre";
+            string FullName = person.FirstName + person.LastName;
+            person.SayName();
+
+            // instantiate class Employee
+            Employee employee = new Employee();
+            employee.FirstName = "Sample ";
+            employee.LastName = "Student";
+            employee.SayName();
+
+        }
+    }
+}
