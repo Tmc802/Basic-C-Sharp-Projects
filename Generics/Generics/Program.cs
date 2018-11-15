a320c4421397b4244bd6f933f936cbda71b66a34
Tyler Mc <tmcintyre802@gmail.com>
2018-11-15 09:02:29 -0700

    Generic Types
    A console app to practice creating and iterating through generic types.

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // instantiate list type string
+            Employee<string> employee = new Employee<string>();
+
+             employee.Things = new List<string> {"List" , "Of", "strings"};
+
+
+             // instantiate list type int
+             Employee<int> employee2 = new Employee<int>();
+
+             employee2.Things = new List<int> {12, 30, 15, 25 };
+
+
+            // iterates string list of things
+            foreach (string item in employee.Things)
+            {
+                Console.WriteLine(item);
+            }
+
+            // iterate int list of things
+            foreach (int item in employee2.Things) {
+                Console.WriteLine(item);
+            }
+        }
+     }
+}
