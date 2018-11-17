e1ac232e9201ef84ddc82c437c56b22ab649759a
Tyler Mc <tmcintyre802@gmail.com>
2018-11-17 16:23:48 -0700

    File I/O
    A console app to demonstrate logging text from an IDE to a file outside of the program

---
+++ File I_Os/filesInsOuts/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace filesInsOuts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string text = Console.ReadLine();
+
+            File.WriteAllText(@"/Users/tmc802/Desktop/Step\ 154\ drill", text);
+
+            Console.WriteLine(@"/Users/tmc802/Desktop/Step\ 154\ drill");
+        }
+    }
+}
