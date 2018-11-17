a731cd916d3817bf5b464213d772c08effc4c987
Tyler Mc <tmcintyre802@gmail.com>
2018-11-17 08:56:14 -0700

    Lambda expressions
    A console app to create lambda expressions to easily search through a list, or create a new one.

---
+++ LambdaExpressions/LambdaExpressions/Employees.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamExpressions
+{
+    public class Employee
+    {
+        // create properties to sort employees by
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create initial list of employees to search through
+            List<Employee> employeeList = new List<Employee>();
+
+                employeeList.Add(new Employee
+                {
+                    FirstName = "Joe",
+                    LastName = "Jonas",
+                    Id = 1
+                });
+
+                employeeList.Add(new Employee
+                {
+                    FirstName = "Benni",
+                    LastName = "Benassi",
+                    Id = 2
+                });
+
+                employeeList.Add(new Employee
+                {
+                    FirstName = "Rick",
+                    LastName = "Ricardo",
+                    Id = 3
+                });
+
+                employeeList.Add(new Employee
+                {
+                    FirstName = "Ethan",
+                    LastName = "Allen",
+                    Id = 4
+                });
+
+                employeeList.Add(new Employee
+                {
+                    FirstName = "Tiger",
+                    LastName = "Woods",
+                    Id = 5
+                });
+
+                employeeList.Add(new Employee
+                {
+                    FirstName = "Conor",
+                    LastName = "McGregor",
+                    Id = 6
+                });
+
+                employeeList.Add(new Employee
+                {
+                    FirstName = "Ryan",
+                    LastName = "Renyolds",
+                    Id = 7
+                });
+
+                employeeList.Add(new Employee
+                {
+                    FirstName = "Joe",
+                    LastName = "McCarthy",
+                    Id = 8
+                });
+
+                employeeList.Add(new Employee
+                {
+                    FirstName = "Joe",
+                    LastName = "Right",
+                    Id = 9
+                });
+
+                employeeList.Add(new Employee
+                {
+                    FirstName = "Allen",
+                    LastName = "Iverson",
+                    Id = 10
+                });
+
+
+
+
+
+            // Create a new list with employees that have the first name Joe
+            List<Employee> employeeJoe = new List<Employee>();
+
+            // use a foreach loop to instantiate a new list
+            foreach (Employee employee in employeeList) {
+                if (employee.FirstName == "Joe") {
+                    employeeJoe.Add(new Employee{FirstName = employee.FirstName, 
+                        LastName = employee.LastName, Id = employee.Id});
+                }
+            }
+
+            Console.WriteLine("Press a key to continue");
+            Console.ReadLine();
+
+
+
+
+
+
+            // instead use a lambda expression to find the employees with the name Joe
+
+            List<Employee> newList = employeeList.Where(x => x.FirstName == "Joe").ToList();
+            foreach (Employee employee in newList)
+            {
+                Console.WriteLine(employee.FirstName + " " + employee.LastName + " " + employee.Id);
+            }
+
+
+
+
+
+
+
+
+
+            // create a list with employee id's greater than 5
+            List<Employee> listIdGreaterThan5 = employeeList.Where(x => x.Id >= 5).ToList();
+
+            //run through list printing them to the console 
+            foreach (Employee employee in listIdGreaterThan5)
+            {
+                Console.WriteLine(employee.FirstName + " " + employee.LastName + " " + employee.Id);
+            }
+
+
+
+            Console.WriteLine("Press a key to continue");
+            Console.ReadLine();
+            
+        }
+    }
+}
