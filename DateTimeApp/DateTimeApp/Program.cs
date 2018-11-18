fa68472bc2d2214c61659805c029f1b386a35330
Tyler Mc <tmcintyre802@gmail.com>
2018-11-17 17:25:09 -0700

    DateTime App
    A console app for working with date time and timespan

---
+++ DateTimeApp/DateTimeApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace DateTimeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("It is currently " + DateTime.Now);
+
+            //prompt user for a number to add onto current DateTime
+            Console.WriteLine("Please enter a number");
+            string userEntry = Console.ReadLine();
+
+            // Convert the string to an int
+            int time = Convert.ToInt32(userEntry);
+            
+            // set current date to variable
+            DateTime date = DateTime.Now;
+
+            // set timespan to new variable and add user entry to new timespan
+            TimeSpan userHour = new TimeSpan (0,time,0,0);
+
+            // combine current date and user entry
+            DateTime result = date + userHour;
+
+            // log result
+            Console.WriteLine(result);
+        }
+    }
+}
+/*
+3. Prints to the console the exact time it will be in X hours, 
+X being the number the user entered in step 2.
+*/
