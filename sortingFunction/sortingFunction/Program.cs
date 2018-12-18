f0d580f8781beaa8089a8e3ee03806a326e18ba2
Tyler Mc <tmcintyre802@gmail.com>
2018-12-18 16:57:30 -0700

    Sorting Function
    A sorting function for strings that keeps count of specific characters and returns a boolean answer as to if the two counts are equal

---
+++ sortingFunction/sortingFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortingFunction
+{
+    class Program :myFunction
+    {
+        static void Main(string[] args)
+        {
+            myFunction function1 = new myFunction();
+            string A = "XXoO";
+            bool result = function1.SortXo(A);
+
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        
+    }
+}
---
+++ sortingFunction/sortingFunction/myFunction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortingFunction
+{
+    class myFunction
+    {
+        public bool SortXo(string A)
+        {
+            //create counters
+            int countX = 0;
+            int countO = 0;
+
+            for(int i = 0; i <= A.Length - 1; i++)
+            {
+                if (A[i] == 'O' || A[i] == 'o')
+                {
+                    countO++;
+                } else if (A[i] == 'X' || A[i] == 'x')
+                {
+                    countX++;
+                }
+            }
+
+            return (countO == countX) ? true:false;
+        }
+    }
+}
