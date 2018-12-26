6b844efe57c6f105e3d5fbd906647fecaaa5e2f7
Tyler Mc <tmcintyre802@gmail.com>
2018-12-25 20:39:31 -0700

    CS file to explain the quicksort algorithim
    A practice file to explain quicksort

---
+++ quicksort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Solution
+    {
+        public static void quicksort (int[] array)
+        {
+            quicksort(array, 0, array.Length - 1);
+        }
+
+        public static void quicksort(int[] array, int left, int right)
+        {
+            if (left >= right)                                                  // if these indexies are out of order
+            {
+                return;
+            }
+            int pivot = array[(left + right) / 2];          // pick the pivot. picks the middle.
+            int index = partition(array, left, right, pivot);  // 
+            quicksort(array, left, index - 1);
+            quicksort(array, index, right);
+
+            
+                }
+        public static int partition(int[] array, int left, int right, int pivot)
+        {
+            while (left <= right)           // move from left to right side, moving pointers closer to each other. 
+            {
+                while (array[left] < pivot)             // move up until a number is found lower than the pivot
+                {
+                    left++;
+                }
+                while (array[right] > pivot)            //move down until a number is found higher than the index
+                {
+                    right--;
+                }
+
+                if(left <= right)               //swap the numbers
+                {
+                    swap(array, left, right);
+                    left++;
+                    right--;
+                }
+            }
+            return left;
+        }
+    }
+}
